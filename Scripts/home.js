d280a6ee5431003b2c5226d216eaa137d405aac3
vagga2U <67494996+vagga2U@users.noreply.github.com>
2023-09-01 15:35:22 +1000

    Updated Calendar
    Added Wannon Zone Tetrathlon to the MPV calendar and event short list.

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,5 +1,5 @@
 
 //on document load, load news item
 $(document).ready(function() {
-    $("#latestNews").load("../pentathlonnews.html #20230615");
+    $("#latestNews").load("../pentathlonnews.html #20230901");
   });
