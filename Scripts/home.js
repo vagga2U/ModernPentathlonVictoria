aa1a279b721bc5cc94e437976be1ef78d8972b42
vagga2U <67494996+vagga2U@users.noreply.github.com>
2024-01-11 19:54:39 +1100

    News Update

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,5 +1,5 @@
 
 //on document load, load news item
 $(document).ready(function() {
-    $("#latestNews").load("../pentathlonnews.html #20231112");
+    $("#latestNews").load("../pentathlonnews.html #20240111");
   });
