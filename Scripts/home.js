b7047d76cb9a78c8bbce63420b9cb51587afff95
vagga2U <67494996+vagga2U@users.noreply.github.com>
2024-06-16 21:00:02 +1000

    Updated Details
    New Committee
    Calendar Update
    Attempted to properly render PDF, currently image instead.

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,5 +1,5 @@
 
 //on document load, load news item
 $(document).ready(function() {
-    $("#latestNews").load("../pentathlonnews.html #20240111");
+    $("#latestNews").load("../pentathlonnews.html #20240616");
   });
